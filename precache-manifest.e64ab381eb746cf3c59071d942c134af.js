bc5cc38ddfbc57997e8158a874cc7b5bad25fcda
Marko Djuric <marko@Markos-iMac.local>
2019-05-21 23:53:17 +0200

    Updates

--- precache-manifest.e64ab381eb746cf3c59071d942c134af.js
+++ precache-manifest.e64ab381eb746cf3c59071d942c134af.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Webshop/static/js/runtime~main.74429dd9.js"
   },
   {
-    "revision": "866e63f70381b61dc4ca",
-    "url": "/Webshop/static/js/main.9b0a3622.chunk.js"
+    "revision": "988dceacf6a6717dde55",
+    "url": "/Webshop/static/js/main.0eaebd15.chunk.js"
   },
   {
     "revision": "7ddbb749ffd673a9f06a",
     "url": "/Webshop/static/js/2.eb5a412c.chunk.js"
   },
   {
-    "revision": "866e63f70381b61dc4ca",
+    "revision": "988dceacf6a6717dde55",
     "url": "/Webshop/static/css/main.10ea7861.chunk.css"
   },
   {
-    "revision": "d4029ec85b601e10b4fb39555a8197d3",
+    "revision": "367bebb7d2808fcadbe3f5826a18d9be",
     "url": "/Webshop/index.html"
   }
 ];
