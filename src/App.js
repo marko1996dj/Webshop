2ff218281339422ba41da7d6a6e309c1fa37aaf0
Marko Djuric <marko@Markos-iMac.local>
2019-05-21 23:52:23 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -37,16 +37,16 @@ class App extends Component {
 				<NavigationItems />
 				<HashRouter>
 					<Switch>
-						<Route path="/" exact component={Webshop} />
-						<Route path="/webshop" component={Webshop} />
-						<Route path="/cart" component={Cart} />
-						<Route path="/wishlist" component={Wishlist} />
-						<Route path="/register" component={Register} />
-						<Route path="/password-reset" component={PassReset} />
-						<Route path="/edit-user" exact component={EditUser} />
-						<Route path="/user-profile" component={UserProfile} />
-						<Route path="/login" render={() => <Login />} />
-						<Route path="/product-page" component={ProductPage} />
+						<Route path="Webshop/#/" exact component={Webshop} />
+						<Route path="Webshop/#/webshop" component={Webshop} />
+						<Route path="Webshop/#/cart" component={Cart} />
+						<Route path="Webshop/#/wishlist" component={Wishlist} />
+						<Route path="Webshop/#/register" component={Register} />
+						<Route path="Webshop/#/password-reset" component={PassReset} />
+						<Route path="Webshop/#/edit-user" exact component={EditUser} />
+						<Route path="Webshop/#/user-profile" component={UserProfile} />
+						<Route path="Webshop/#/login" render={() => <Login />} />
+						<Route path="Webshop/#/product-page" component={ProductPage} />
 					</Switch>
 				</HashRouter>
 			</React.Fragment>
